3a9785ded6c9a8fe82ba2df67184dd90718397f6
colasonic <colasonic@gmail.com>
2023-10-02 14:46:09 -0400

    my changes 20230929

--- keyboards/handwired/dactyl_manuform/5x6/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6/rules.mk
@@ -11,3 +11,5 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes
+TAP_DANCE_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
